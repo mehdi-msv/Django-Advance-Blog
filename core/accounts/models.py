29ada58c6b68d91a36ea745f0182883a3171b64e
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-05 23:20:46 +0330

    Create UserManager and Update User Model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,11 +1,47 @@
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,PermissionsMixin,BaseUserManager)
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password and extra fields.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractBaseUser,PermissionsMixin):
+    '''
+    Custom User Model for Accounts app
+    '''
     email = models.EmailField(max_length=256,unique=True)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     # is_verified = models.BooleanField(default=False)
