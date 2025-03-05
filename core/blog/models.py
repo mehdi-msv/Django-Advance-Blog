6b1cc3c33e6b1841ab3876d5335112502609151a
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-05 20:57:01 +0330

    Create primary model for posts

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    '''
+    this is a class to define posts for blog app
+    '''
+    
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ImageField(blank=True,null=True)
+    category = models.ForeignKey("Category",on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    status = models.BooleanField()
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
