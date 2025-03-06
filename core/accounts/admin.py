1f798813445f0bd9813cc1fc3533b3935ad934a1
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-06 16:07:02 +0330

    Update admin.py

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('email','is_superuser', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ("email",)
+    
+    fieldsets = (
+        ('Authentications', {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active')}),
+        ('Group Permissions', {'fields': ('groups','user_permissions')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+    
+    add_fieldsets = (
+        ("Registration", {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2',),
+        }),
+        ("Permissions", {
+            'classes': ('wide',),
+            'fields': ('is_superuser', 'is_staff', 'is_active'),
+        }))
+
+admin.site.register(User, CustomUserAdmin)
