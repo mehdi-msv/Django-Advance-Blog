07eb4a2a4d59ec652647c88e0957dfcbcdb9d6ab
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-07 17:57:10 +0330

    Create IndexView Class that inherits from TemplateView class

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
 from django.views.generic import TemplateView
-from .views import fbv_index
+from . import views
 
 urlpatterns = [
-    path('fbv-index', fbv_index, name='fbv_index'),
-    path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name':'mehdi'}),name='cbv_index'),
+    path('fbv-index', views.fbv_index, name='fbv_index'),
+#    path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name':'mehdi'}),name='cbv_index'),
+    path('cbv-index', views.IndexView.as_view(), name='cbv_index'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Post
 # Create your views here.
 def fbv_index(request):
     context = {'name':'mehdi'}
     return render(request, 'index.html',context)
+
+class IndexView(TemplateView):
+    '''
+    This is a class-based view for index page
+    '''
+    template_name = 'index.html'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'mehdi'
+        context['posts'] = Post.objects.all()
+        return context
