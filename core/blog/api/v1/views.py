db0047996d124eaa9a65632cb0133cbf3c1eff10
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-28 20:47:41 +0330

    set not found(404) error for post api view

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 @api_view()
 def postList(request):
     return Response('ok')
@@ -9,6 +11,12 @@ def postList(request):
 
 @api_view()
 def postDetail(request,id):
-    post = Post.objects.get(pk=id)
+    # try:
+    #     post = Post.objects.get(pk=id)
+    #     serializer = PostSerializer(post)
+    #     return Response(serializer.data)
+    # except Post.DoesNotExist:
+    #     return Response('{detail:not found}', status=status.HTTP_404_NOT_FOUND)
+    post = get_object_or_404(Post,pk=id)
     serializer = PostSerializer(post)
     return Response(serializer.data)
