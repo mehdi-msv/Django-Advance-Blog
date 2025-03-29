d043eaad86f3c76ef3804953edc6abf5e9db4d37
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-29 23:04:09 +0330

    Update views.py

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -140,3 +140,5 @@ class PostViewSet(ModelViewSet):
         return super().update(request, *args, **kwargs)
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
