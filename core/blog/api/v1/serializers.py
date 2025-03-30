0ad09f49f6a209cee7a1dd14cc9bf5a19ac11dd2
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-30 05:42:34 +0330

    Separate category that displays and the one that receives
    Receives: choose from several displayed categories.
    Display as name and ID.

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -18,10 +18,9 @@ class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField()
-    category = CategorySerializer()
     class Meta:
         model = Post
-        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'absolute_url' , 'snippet' ,'created_date', 'published_date']
+        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'category' ,'absolute_url' , 'snippet' ,'created_date', 'published_date']
         # read_only_fields = ['content']
 
     def get_absolute_url(self,obj):
@@ -29,6 +28,6 @@ class PostSerializer(serializers.ModelSerializer):
         absolute_url = obj.get_absolute_api_url()
         return request.build_absolute_uri(absolute_url)
     def to_representation(self, instance):
-        
-        return super().to_representation(instance)
-        
+        repo = super().to_representation(instance)
+        repo['category'] = CategorySerializer(instance.category).data
+        return repo
