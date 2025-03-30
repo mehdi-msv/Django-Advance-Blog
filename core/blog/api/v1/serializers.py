f5f76a8747b1349ab49bca6681e360ce7f9ee75c
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-30 05:15:20 +0330

    change how to show category in post api page

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -5,12 +5,20 @@ from ...models import Post, Category
 #     title = serializers.CharField(max_length=255)
 #     id = serializers.IntegerField()
 
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+        
+        
 class PostSerializer(serializers.ModelSerializer):
     # content = serializers.CharField(read_only=True)
     # content = serializers.ReadOnlyField()
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     absolute_url = serializers.SerializerMethodField()
+    category = CategorySerializer()
     class Meta:
         model = Post
         fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'absolute_url' , 'snippet' ,'created_date', 'published_date']
@@ -20,9 +28,7 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         absolute_url = obj.get_absolute_api_url()
         return request.build_absolute_uri(absolute_url)
+    def to_representation(self, instance):
         
-class CategorySerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Category
-        fields = ['id', 'name']
+        return super().to_representation(instance)
+        
