c3214f591be6d1e3baeb2f05abe2dc2f5eaeaddc
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-30 04:55:53 +0330

    add get absolute url to blog api-v1 serializer for getting post url

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -10,12 +10,17 @@ class PostSerializer(serializers.ModelSerializer):
     # content = serializers.ReadOnlyField()
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'snippet' ,'created_date', 'published_date']
+        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'absolute_url' , 'snippet' ,'created_date', 'published_date']
         # read_only_fields = ['content']
 
-
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        absolute_url = obj.get_absolute_api_url()
+        return request.build_absolute_uri(absolute_url)
+        
 class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
