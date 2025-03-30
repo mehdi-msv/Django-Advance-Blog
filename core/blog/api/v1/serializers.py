326536e631faca53ee9a5cb63e1c0a24a623562a
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-30 04:10:00 +0330

    add snippet and relative_url to api-v1 serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -8,9 +8,11 @@ from ...models import Post, Category
 class PostSerializer(serializers.ModelSerializer):
     # content = serializers.CharField(read_only=True)
     # content = serializers.ReadOnlyField()
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'author','title', 'content', 'status', 'created_date', 'published_date']
+        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'snippet' ,'created_date', 'published_date']
         # read_only_fields = ['content']
 
 
