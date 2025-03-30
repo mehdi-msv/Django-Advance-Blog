0ee9f6495f03dd8384197bd21227272435996b45
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-03-30 06:14:20 +0330

    Use to_representation for separating fields that can be displayed in post-detail and post-list

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -17,10 +17,14 @@ class PostSerializer(serializers.ModelSerializer):
     # content = serializers.ReadOnlyField()
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url', read_only=True)
-    absolute_url = serializers.SerializerMethodField()
+    absolute_url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Post
-        fields = ['id', 'author','title', 'content', 'status', 'relative_url' , 'category' ,'absolute_url' , 'snippet' ,'created_date', 'published_date']
+        fields = [
+            'id', 'author', 'image' ,'title', 'content', 'status',
+            'relative_url' , 'absolute_url' , 'category'  , 'snippet' ,
+            'created_date', 'published_date'
+            ]
         # read_only_fields = ['content']
 
     def get_absolute_url(self,obj):
@@ -28,6 +32,13 @@ class PostSerializer(serializers.ModelSerializer):
         absolute_url = obj.get_absolute_api_url()
         return request.build_absolute_uri(absolute_url)
     def to_representation(self, instance):
+        request = self.context.get('request')
         repo = super().to_representation(instance)
         repo['category'] = CategorySerializer(instance.category).data
+        if request.parser_context.get('kwargs').get('pk'):
+            repo.pop('snippet', None)
+            repo.pop('absolute_url', None)
+            repo.pop('relative_url', None)
+        else:
+            repo.pop('content')
         return repo
