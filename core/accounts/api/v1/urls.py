77ee4f3e14f1e3f8f2885c103f9d78ef059ef525
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-04 01:56:22 +0330

    feat(auth): implement user registration API endpoint
    Added registration view and serializer

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
-
+from .views import RegistrationApiView
 
 
 app_name = 'api-v1'
 
 
 urlpatterns = [
+    path('registration/', RegistrationApiView.as_view(), name='registration')
 
 ]
