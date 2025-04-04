2e36d6870ca0b0f4e12b0e79964dd031825e6fc2
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-05 00:20:35 +0330

    fix profiles.py importing missed

--- core/accounts/models/profiles.py
+++ core/accounts/models/profiles.py
@@ -1,6 +1,7 @@
 from django.db import models
 from .users import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 class Profile(models.Model):
     '''
     Custom Profile Model for Accounts app
