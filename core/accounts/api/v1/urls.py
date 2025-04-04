669b14ec5d9eb94407eaf4520d1970984110a7fc
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-04 15:37:17 +0330

    Add JWT authentication endpoints to API
    - Installed djangorestframework-simplejwt for JWT authentication.
    - Added JWT create, refresh, and verify endpoints.
    - Integrated JWT authentication with the project for better security.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 # from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name = 'api-v1'
@@ -9,4 +14,7 @@ urlpatterns = [
     path('registration/', views.RegistrationApiView.as_view(), name='registration'),
     path('token/login/', views.CustomObtainAuthToken.as_view(), name='token-login'),
     path('token/logout/', views.DiscardAuthToken.as_view(), name='token-logout'),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
