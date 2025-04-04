198817f8271257bf0f9097434eca81b6601d3bce
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-05 00:06:38 +0330

    Separating models from each other

---
+++ core/accounts/models/profiles.py
@@ -0,0 +1,23 @@
+from django.db import models
+from .users import User
+
+class Profile(models.Model):
+    '''
+    Custom Profile Model for Accounts app
+    '''
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    image = models.ImageField(blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
+    
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
