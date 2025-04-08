8763f2578e5e12d023967e418946ccba00894024
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-09 01:47:08 +0330

    Add test case for blog index URL
    Added a test case to ensure the blog index URL is correctly resolved to the IndexView.

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,12 @@
-# from django.test import TestCase
-
+from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import IndexView
 # Create your tests here.
+
+class TestUrl(TestCase):
+
+    def test_url_exists_at_correct_location(self):
+        url = reverse("blog:index")
+        self.assertEquals(resolve(url).func.view_class, IndexView)
+        
+        
