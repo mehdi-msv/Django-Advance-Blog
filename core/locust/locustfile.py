0318baa9c3c1ccb3ece59f0f75130ec02098bfe1
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2025-04-15 15:49:39 +0330

    Add blog post and category endpoint tasks to Locust test file
    Updated QuickstartUser class in locustfile.py to include performance tests for:
    
    GET /blog/api/v1/post/
    
    GET /blog/api/v1/category/

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,11 @@
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    pass
+
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+
+    @task
+    def post_category(self):
+        self.client.get("/blog/api/v1/category/")
